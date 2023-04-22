696d0bc2d5f455f34e2f0b37b0f0010c37ee4122
flyajar <rj.jovenal@gmail.com>
2023-04-22 19:42:59 +0800

    added the reserved words

---
+++ reserved-words.py
@@ -0,0 +1,20 @@
+# pass
+print('pass')
+for num in range(0, 10):
+    if num == 5:
+        pass
+    print(num)
+
+# break
+print('break')
+for num in range(0, 10):
+    if num == 5:
+        break
+    print(num)
+
+# continue
+print('continue')
+for num in range(0, 10):
+    if num == 5:
+        continue
+    print(num)
