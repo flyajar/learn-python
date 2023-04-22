f05acb61be61d03d25e72c7eff1efade4287ab74
flyajar <rj.jovenal@gmail.com>
2023-04-22 19:23:32 +0800

    added the operators

---
+++ operators.py
@@ -0,0 +1,79 @@
+# arithmetic operators
+
+number1 = 4
+number2 = 2
+
+# addition
+print(number1 + number2)
+
+# subtraction
+print(number1 - number2)
+
+# multiplication
+print(number1 * number2)
+
+# division
+print(number1 / number2)
+
+# modulus
+# remainder of the number
+print(number1 % number2)
+
+# exponentiation
+print(number1 ** number2)
+
+# comparison operators
+
+# equal to
+print(number1 == number2)
+
+# not equal to
+print(number1 != number2)
+
+# greater than
+print(number1 > number2)
+
+# less than
+print(number1 < number2)
+
+# greater than or equal to
+print(number1 >= number2)
+
+# less than or equal to
+print(number1 <= number2)
+
+
+# logical operators
+
+a = True
+b = False
+c = True
+
+# AND
+print(a and c)
+
+# OR
+print(a or b)
+
+# NOT
+print(not a)
+print(not b)
+
+# membership operators
+fruits = ('apple', 'banana', 'orange')
+
+# IN
+print('apple' in fruits)
+
+# NOT IN
+print('mango' not in fruits)
+
+# identity operators
+a = 10
+b = 10
+
+# is
+print(a is b)
+
+# is not
+print(a is not b)
