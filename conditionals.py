0f2c4c35b369870497728ee8e3144d9cf8ceb832
flyajar <rj.jovenal@gmail.com>
2023-04-22 17:51:18 +0800

    added the conditional

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+# if statement
+
+number = 10
+
+if number > 0:
+    print(f'The number {number} is greater than 0')
+elif number == 10:
+    print(f'The number is exactly {number}')
+else:
+    print(f'The number {number} is less than 0')
