191d2c532645ddcac7d370c5d021f870c38afe7a
flyajar <rj.jovenal@gmail.com>
2023-04-22 18:34:32 +0800

    added the loop

---
+++ loops.py
@@ -0,0 +1,16 @@
+# for loop
+
+numbers = [1, 2, 3, 4, 5]
+
+for number in numbers:
+    print(number)
+
+
+print("=======")
+
+# while loop
+
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
