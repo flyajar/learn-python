126cdc23d0b2d052639170ae7ab76b20eb31ac00
flyajar <rj.jovenal@gmail.com>
2023-04-22 19:59:51 +0800

    added more examples in built in functions

--- built-in-functions.py
+++ built-in-functions.py
@@ -2,6 +2,21 @@
 
 numbers = [1, 2, 3, 4, 5]
 
-# print(len(numbers))
+print(len(numbers))
+
 for i in range(1, 5):
     print(i)
+
+print(sum(numbers))
+
+squared_numbers = map(lambda x: x ** 2, numbers)
+
+print(f'result when numbers are squared: {list(squared_numbers)}')
+
+even_numbers = filter(lambda num: num % 2 == 0, numbers)
+
+print(f'even numbers are {list(even_numbers)}')
+
+print(f'the smallest number is {min(numbers)}')
+
+print(f'the biggest number is {max(numbers)}')
