91c53fd3c0750d4769f1f6865a2e1d47e2544e2a
flyajar <rj.jovenal@gmail.com>
2023-04-23 11:40:39 +0800

    added the string functions

---
+++ string-functions.py
@@ -0,0 +1,21 @@
+word = 'hello'
+
+# len
+print(len(word))
+
+# str.upper
+print(word.upper())
+
+# str.lower
+print(word.lower())
+
+# str.strip
+print(word.strip())
+
+# str.split
+print(word.split('l'))
+
+# str.replace
+print(word.replace('hello', 'bacon'))
+
+
