dcbed0dcb0ccef94d004dfd5ee13417bf1fe83b4
flyajar <rj.jovenal@gmail.com>
2023-04-28 19:07:00 +0800

    added the keyword arguments option of a function

--- functions.py
+++ functions.py
@@ -18,3 +18,12 @@ def number(num: int):
 
 
 number(1)
+
+
+# keyword arguments
+
+def describe_pet(animal_type, animal_name):
+    print(f'\nI have a {animal_type}')
+    print(f'\nMy {animal_type}\'s name is {animal_name}')
+
+describe_pet(animal_name='Fyodor', animal_type='cat')
